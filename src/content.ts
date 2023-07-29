ea33ba7a9e5d42ef8586b2865d7db27225fccba3
Yoshihiro Tanaka <contact@cordea.jp>
2023-07-29 11:34:32 +0900

    add inputs

--- src/content.ts
+++ src/content.ts
@@ -1,15 +1,50 @@
-const node = document.querySelector('#sort-select-menu')
+const filterName = 'date-select-menu'
+
+const node = document.querySelector('#author-select-menu')
 const container = node.parentNode
 
 const filter = node.cloneNode() as Element
-const summary = node.querySelector('summary').cloneNode(true)
 
+const summary = createSummary()
 const details = document.createElement('details-menu')
-const modal = document.createElement('div')
+details.setAttribute('class', 'SelectMenu SelectMenu--hasFilter right-0')
+const modal = createModal()
 details.appendChild(modal)
 
-filter.id = 'date-select-menu'
-filter.appendChild(summary)
-filter.appendChild(details)
+filter.id = filterName
+filter.append(summary, details)
 
 container.appendChild(filter)
+
+function createSummary(): Element {
+    const summary = document.createElement('summary')
+    summary.setAttribute('class', 'btn-link')
+    const caret = document.createElement('span')
+    caret.setAttribute('class', 'dropdown-caret hide-sm')
+    summary.append(document.createTextNode(' Date '), caret)
+    return summary
+}
+
+function createInput(): Element {
+    const input = document.createElement('input')
+    input.setAttribute('class', 'SelectMenu-input form-control')
+    return input
+}
+
+function createModal(): Element {
+    const modal = document.createElement('div')
+    modal.setAttribute('class', 'SelectMenu-modal')
+
+    const header = node.querySelector('.SelectMenu-header').cloneNode(true) as Element
+    header.querySelector('.SelectMenu-title').innerHTML = 'Filter by'
+    header.querySelector('.SelectMenu-closeButton').setAttribute('data-toggle-for', this.filterName)
+
+    const body = document.createElement('div')
+    body.setAttribute('class', 'SelectMenu-filter')
+    const startInput = createInput()
+    const endInput = createInput()
+    body.append(startInput, endInput)
+
+    modal.append(header, body)
+    return modal
+}
