6fcc8144c23855b2b52be6290a09650087b61820
Yoshihiro Tanaka <contact@cordea.jp>
2023-07-30 11:51:35 +0900

    add choices

--- src/content.ts
+++ src/content.ts
@@ -24,6 +24,15 @@ function createInput(id: string, label: string): HTMLElement {
   return container;
 }
 
+function createItem(id: string, label: string): HTMLElement {
+  const item = document.createElement("a");
+  item.setAttribute("class", "SelectMenu-item");
+  const icon = node.querySelector(".SelectMenu-icon").cloneNode(true);
+  item.append(icon, label);
+  item.id = id;
+  return item;
+}
+
 function createModal(): Element {
   const modal = document.createElement("div");
   modal.setAttribute("class", "SelectMenu-modal");
@@ -35,46 +44,117 @@ function createModal(): Element {
   header.querySelector(".SelectMenu-closeButton").id =
     "GitHubFilter-closeButton";
 
-  const body = document.createElement("div");
-  body.setAttribute("class", "SelectMenu-filter");
-  const startInput = createInput(
-    "GitHubFilter-startDate",
-    "Start date (YYYY-MM-DD): ",
-  );
-  startInput.style.setProperty("margin-bottom", "16px");
-  const endInput = createInput(
-    "GitHubFilter-endDate",
-    "End date (YYYY-MM-DD): ",
-  );
-  body.append(startInput, endInput);
+  const list = document.createElement("div");
+  list.setAttribute("class", "SelectMenu-list");
+  const closed = createItem("GitHubFilter-closed", "Closed");
+  const merged = createItem("GitHubFilter-merged", "Merged");
+  list.append(closed, merged);
 
-  modal.append(header, body);
+  const divider = document.createElement("div");
+  divider.append(document.createTextNode("Date (YYYY-MM-DD)"));
+  divider.setAttribute("class", "SelectMenu-divider");
+
+  const filter = document.createElement("div");
+  filter.setAttribute("class", "SelectMenu-filter");
+  const startInput = createInput("GitHubFilter-startDate", "Start: ");
+  startInput.style.setProperty("margin-bottom", "8px");
+  const endInput = createInput("GitHubFilter-endDate", "End: ");
+  filter.append(startInput, endInput);
+
+  modal.append(header, list, divider, filter);
   return modal;
 }
 
-function updateFilter(key: string, value: string) {
+function updateFilter(key: string, value: string): boolean {
   const current = input.value;
   const next = value.length > 0 ? `${key}:${value}` : "";
-  if (current.indexOf(`${key}:`) > 0) {
+  if (current.indexOf(`${key}:`) >= 0) {
     input.value = current.replace(RegExp(`${key}:[\\w\.]+`), next);
-    return;
+    return true;
   }
-  input.value += current.endsWith(" ") ? next : ` ${next}`;
+  if (next) {
+    input.value += current.endsWith(" ") ? next : ` ${next}`;
+    return true;
+  }
+  return false;
 }
 
-function onDateChanged(start: string, end: string) {
-  let q: string;
-  if (start.length <= 0 && end.length <= 0) {
-    q = "";
-  } else if (start.length <= 0) {
-    q = end;
-  } else if (end.length <= 0) {
-    q = start;
+function onChanged(
+  start: string,
+  end: string,
+  closed: boolean,
+  merged: boolean,
+) {
+  let key: string;
+  if (closed) {
+    key = "closed";
+  } else if (merged) {
+    key = "merged";
   } else {
-    q = `${start}..${end}`;
+    return;
   }
-  updateFilter("opened", q);
-  form.submit();
+  let value: string;
+  if (start.length <= 0 && end.length <= 0) {
+    value = "";
+  } else if (start.length <= 0) {
+    value = end;
+  } else if (end.length <= 0) {
+    value = start;
+  } else {
+    value = `${start}..${end}`;
+  }
+  if (updateFilter(key, value)) {
+    form.submit();
+  }
+}
+
+function observe() {
+  const closed = document.querySelector("#GitHubFilter-closed");
+  const merged = document.querySelector("#GitHubFilter-merged");
+  const startDate: HTMLInputElement = document.querySelector(
+    "#GitHubFilter-startDate",
+  );
+  const endDate: HTMLInputElement = document.querySelector(
+    "#GitHubFilter-endDate",
+  );
+  closed.addEventListener("click", (_) => {
+    const checked = closed.ariaChecked;
+    if (checked === "true") {
+      closed.ariaChecked = "false";
+      onChanged(startDate.value, endDate.value, false, false);
+      return;
+    }
+    closed.ariaChecked = "true";
+    merged.ariaChecked = "false";
+    onChanged(startDate.value, endDate.value, true, false);
+  });
+  merged.addEventListener("click", (_) => {
+    const checked = merged.ariaChecked;
+    if (checked === "true") {
+      merged.ariaChecked = "false";
+      onChanged(startDate.value, endDate.value, false, false);
+      return;
+    }
+    merged.ariaChecked = "true";
+    closed.ariaChecked = "false";
+    onChanged(startDate.value, endDate.value, false, true);
+  });
+  startDate.addEventListener("change", (_) => {
+    onChanged(
+      startDate.value,
+      endDate.value,
+      closed.ariaChecked === "true",
+      merged.ariaChecked === "true",
+    );
+  });
+  endDate.addEventListener("change", (_) => {
+    onChanged(
+      startDate.value,
+      endDate.value,
+      closed.ariaChecked === "true",
+      merged.ariaChecked === "true",
+    );
+  });
 }
 
 const container = node.parentNode;
@@ -94,17 +174,4 @@ container
   .querySelector("#GitHubFilter-closeButton")
   .setAttribute("data-toggle-for", filterName);
 
-const startDate: HTMLInputElement = document.querySelector(
-  "#GitHubFilter-startDate",
-);
-const endDate: HTMLInputElement = document.querySelector(
-  "#GitHubFilter-endDate",
-);
-startDate.addEventListener("change", (e) => {
-  const value = (e.target as HTMLInputElement).value;
-  onDateChanged(value, endDate.value);
-});
-endDate.addEventListener("change", (e) => {
-  const value = (e.target as HTMLInputElement).value;
-  onDateChanged(startDate.value, value);
-});
+observe();
